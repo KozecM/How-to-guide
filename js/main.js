69cab8c32dcc68fcb3667ff04d50c2055d47fa3b
KozecM <kozecm@onid.oregonstate.edu>
2016-05-18 15:38:00 -0700

    things
    added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+var requestify = require('requestify');
 
+requestify.get('http://api.brewerydb.com/v2/beer/Yq3v6n?key=57ac3d20257053737a469c6962fd60be').then(function(response) {
+    // Get the response body (JSON parsed or jQuery object for XMLs)
+    response.getBody();
+    console.log(response);
+    });
