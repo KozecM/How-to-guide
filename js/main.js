6e6f53187b9681866d470e7f6181a0dd52c15c73
KozecM <kozecm@onid.oregonstate.edu>
2016-05-19 15:26:38 -0700

    change

--- js/main.js
+++ js/main.js
@@ -33,3 +33,21 @@ app.get('/',function(req, res){
 app.get('/',function(req, res){
 	res.render('index');
 });
+
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
